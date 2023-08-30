43399f58429b8812b9b812e42430570175e7130b
Eric Richter <erichte@linux.ibm.com>
2023-08-30 13:28:21 -0500

    just have x86 do the build
    Signed-off-by: Eric Richter <erichte@linux.ibm.com>

--- ci/build.sh
+++ ci/build.sh
@@ -16,10 +16,19 @@ mkdir -p /root/rpmbuild/SOURCES
 cp secvarctl-${VERSION}.tar.gz /root/rpmbuild/SOURCES
  
 # Run Build
-rpmbuild -ba secvarctl.spec
+if [[ "x86_64" == $(uname -m) ]]; then
+  # Only one srpm is needed, so just arbitrarily pick the faster x86_64 build to do it
+  rpmbuild -ba secvarctl.spec
+else
+  rpmbuild -bb secvarctl.spec
+fi
 
 # Move generated RPMs out of container
 mkdir -p rpms
 cp /root/rpmbuild/RPMS/*/*.rpm rpms/
-cp /root/rpmbuild/SRPMS/*.rpm rpms/
+if [[ "x86_64" == $(uname -m) ]]; then
+  # Only the x86_64 build generates the srpm
+  cp /root/rpmbuild/SRPMS/*.rpm rpms/
+fi
+
 # Note: srpm s the same for all arches, so really only the first to complete needs to be done
