dc95373ca0f542b69c25aa1a3c6ba30836a679fd
Eric Richter <erichte@linux.ibm.com>
2023-08-30 14:04:17 -0500

    release deps

--- ci/build.sh
+++ ci/build.sh
@@ -4,11 +4,6 @@ VERSION=1.0
 
 set -e
 
-# if [ -z ${ARCH}]; then
-#   echo "ARCH must be set to a valid rpm build architecture"
-#   exit 1
-# fi
-
 # Generate source tarball
 ln -s . secvarctl-${VERSION}
 tar czf secvarctl-${VERSION}.tar.gz secvarctl-${VERSION}/*
