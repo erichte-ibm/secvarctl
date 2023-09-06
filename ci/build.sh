087bc8cc77a1a14e289244870eae7a035e2f3c70
Eric Richter <erichte@linux.ibm.com>
2023-09-06 15:25:37 -0500

    use consistent rpmbuild root directory
    Signed-off-by: Eric Richter <erichte@linux.ibm.com>

--- ci/build.sh
+++ ci/build.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
 VERSION=1.0
+RPMBUILD_ROOT=~/rpmbuild
 
 set -e
 
 # Generate source tarball
 ln -s . secvarctl-${VERSION}
 tar czf secvarctl-${VERSION}.tar.gz secvarctl-${VERSION}/*
-mkdir -p /root/rpmbuild/SOURCES
+mkdir -p ${RPMBUILD_ROOT}/SOURCES
 cp secvarctl-${VERSION}.tar.gz /root/rpmbuild/SOURCES
  
 # Run Build
 if [[ "x86_64" == $(uname -m) ]]; then
   # Only one srpm is needed, so just arbitrarily pick the faster x86_64 build to do it
-  rpmbuild -ba secvarctl.spec
+  rpmbuild --root ${RPMBUILD_ROOT} -ba secvarctl.spec
 else
-  rpmbuild -bb secvarctl.spec
+  rpmbuild --root ${RPMBUILD_ROOT} -bb secvarctl.spec
 fi
 
 # Move generated RPMs out of container
 mkdir -p rpms
-cp /root/rpmbuild/RPMS/*/*.rpm rpms/
+cp ${RPMBUILD_ROOT}/RPMS/*/*.rpm rpms/
 if [[ "x86_64" == $(uname -m) ]]; then
   # Only the x86_64 build generates the srpm
-  cp /root/rpmbuild/SRPMS/*.rpm rpms/
+  cp ${RPMBUILD_ROOT}/SRPMS/*.rpm rpms/
 fi
 
 # Note: srpm s the same for all arches, so really only the first to complete needs to be done
